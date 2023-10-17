2dabace217bd35278028eca04ef249a1ce71037a
Fabian Daugs <fabian.daugs@applysia.de>
2023-10-17 13:22:05 +0200

    Update package metadata

--- test/channel_id_test.dart
+++ test/channel_id_test.dart
@@ -1,20 +1,20 @@
+import 'package:action_cable_plus/channel_id.dart';
 import 'package:test/test.dart';
-import 'package:action_cable/channel_id.dart';
 
 void main() {
   group("ChannelId", () {
     test("it should correctly encode channel id", () {
-      final channelId = encodeChannelId("Chat", {"id": 1}).toString();
+      final channelId = encodeChannelId("Chat", {"id": 1});
       expect('{"channel":"ChatChannel","id":1}', channelId);
     });
 
     test("it should correctly encode channel id", () {
-      final channelId = encodeChannelId("ChatChannel", {"id": 1}).toString();
+      final channelId = encodeChannelId("ChatChannel", {"id": 1});
       expect('{"channel":"ChatChannel","id":1}', channelId);
     });
 
     test("it should correctly parse channelId", () {
-      final channelIdJSON = '{"id": 1, "channel": "NotificationChannel"}';
+      const channelIdJSON = '{"id": 1, "channel": "NotificationChannel"}';
       final channelId = parseChannelId(channelIdJSON);
       expect('{"channel":"NotificationChannel","id":1}', channelId);
     });
